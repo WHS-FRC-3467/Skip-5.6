b797a8f10c54bc1bec6370c14ca983f75c2ea113
Starguy18 <hrouillard@team3467.org>
2016-02-18 15:30:58 -0500

    Intake and OI Stuffs

--- src/org/usfirst/frc/team3467/robot/subsystems/Intake/commands/Roll_Out.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/Intake/commands/Roll_Out.java
@@ -2,40 +2,37 @@ package org.usfirst.frc.team3467.robot.subsystems.Intake.commands;
 
 import org.usfirst.frc.team3467.robot.commands.CommandBase;
 
-public class Roll_In extends CommandBase {
+public class Roll_Out extends CommandBase {
 
 	private boolean FAST;
 	
-	public Roll_In(boolean fast) {
+	public Roll_Out(boolean fast) {
 		requires(INTAKE);
 		FAST = fast;
 	}
 	
 	protected void initialize() {
-		
+
 	}
 
 	protected void execute() {
 		if (FAST) {
-			INTAKE.intakeFast();
+			INTAKE.outakeFast();
 		}
 		else {
-			INTAKE.intakeSlow();
+			INTAKE.outakeSlow();
 		}
 	}
 
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	protected void end() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 		
 	}
 }
