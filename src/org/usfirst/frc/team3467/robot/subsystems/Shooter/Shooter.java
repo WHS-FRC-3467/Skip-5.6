ce8641f2881e42fea385572f53578832869af9bd
Scott Kukshtel <scott@kukshtel.com>
2016-02-19 16:32:22 -0500

    Shooter Updates

--- src/org/usfirst/frc/team3467/robot/subsystems/Shooter/Shooter.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/Shooter/Shooter.java
@@ -1,75 +1,191 @@
 package org.usfirst.frc.team3467.robot.subsystems.Shooter;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team3467.robot.RobotMap;
+import org.usfirst.frc.team3467.robot.subsystems.Shooter.commands.ShooterReset;
 
-public class Shooter extends PIDSubsystem{
+/*
+	How the Shooter works:
+	
+	Manual mode:  Holding left bumper on gamepad, left stick Y axis controls
+	the reset bar.
+	
+	PID mode: PID loop runs continuously and responds to changes in setpoint 
+	made by calls to latch() and clear()
+ 
+ */
+
+
+public class Shooter extends PIDSubsystem {
+
+	// Controls display to SmartDashboard
+	private static final boolean debugging = true;
 
 	//Catapult Objects
-	public CANTalon can;
-	public DoubleSolenoid sun;
-	public AnalogPotentiometer pot;
-	public AnalogInput ai;
+	public CANTalon resetBar;
+	public DoubleSolenoid catLatch;
+	public AnalogPotentiometer resetAngle;
 	
-	//PID Constant
-	private final double Shoot_P = 0.0;
-	private final double Shoot_I = 0.0;
-	private final double Shoot_D = 0.0;
-	private final double Shoot_F = 0.0;
+	//PID Constants
+	private static final double SHOOT_P = 0.0;
+	private static final double SHOOT_I = 0.0;
+	private static final double SHOOT_D = 0.0;
+	
+	private static final double TOLERANCE = 0.1;
+	
+	// PID variables
+	double m_resetBarSetpoint;
+	boolean m_usePID;
+
+	// Reset bar setpoints
+	private double clearPoint = 0.0;  // bar is out of the way of catapult
+	private double latchPoint = 5.0; // bar is holding catapult so it can be latched
 
 	private Shooter instance;
 	
 	//Shooter Constructor
 	public Shooter() {
-		super("Shooter", 0.0, 0.0, 0.0);
+
+		super("Shooter", SHOOT_P, SHOOT_I, SHOOT_D);
 
 		instance = this;
-		
-		ai = new AnalogInput(RobotMap.catapult_analog_input);
-		pot = new AnalogPotentiometer(ai);
-		can = new CANTalon(RobotMap.catapult_Talon);
-		sun = new DoubleSolenoid(RobotMap.catapult_solenoid_latch, 
-								RobotMap.catapult_solenoid_release);
+
+		resetAngle = new AnalogPotentiometer(new AnalogInput(RobotMap.catapult_potentiometer_port));
+		resetBar = new CANTalon(RobotMap.catapult_Talon);
+		catLatch = new DoubleSolenoid(RobotMap.catapult_solenoid_latch, RobotMap.catapult_solenoid_release);
+
+		m_resetBarSetpoint = resetAngle.get();
+		m_usePID = true;
+		this.setAbsoluteTolerance(TOLERANCE);
 	}
 		
+	protected void initDefaultCommand() {
+		this.setDefaultCommand(new ShooterReset());	// Drive Manually
+//		this.setDefaultCommand(new ShooterReset(true));		// Use PID
+	}
+	
 	//Returns instance of Shooter Subsystem
 	public Shooter getInstance() {
 		return instance;
 	}
 	
-	//Set solenoid value
-	public void setSun(Value actuate) {
-		sun.set(actuate);
-	}
-	
-	//Sets CANTalon Value
-	public void setCan (double voot) {
-		can.set(voot);
-	}
-
 	public void SetPoint(double setpoint) {
 		this.setSetpoint(setpoint);
 	}
 	
+	public void initManualMode() {
+
+		if (m_usePID) {
+			m_usePID = false;
+			this.disable();
+			
+			// Stop motor until we are ready to set speed
+			resetBar.set(0);
+			
+			if (debugging)
+		    	SmartDashboard.putBoolean("Shooter PID Enabled", false);
+		}
+	}
 	
+	public void driveManual(double speed) {
+		
+		// If trying to drive and a limit switch is hit, then stop...
+		if(resetBar.isFwdLimitSwitchClosed() && speed > 0.0) {
+			speed = 0.0;
+		}
+		else if(resetBar.isRevLimitSwitchClosed() && speed < 0.0) {
+			speed = 0.0;
+		}
+		else
+		{
+			resetBar.set(speed);	
+		}
+
+		// Update the reset bar setpoint even while in manual mode
+		// to avoid surprises when returning to PID control
+		double angle = resetAngle.get();
+		if (debugging) {
+	    	SmartDashboard.putNumber("Shooter Reset Angle", angle);
+		}
+		m_resetBarSetpoint = angle;
+	}
+
+	public boolean initPIDMode() {
+
+		if (!m_usePID) {
+			m_usePID = true;
+
+			this.setSetpoint(m_resetBarSetpoint);
+			this.enable();
+		
+			if (debugging)
+		    	SmartDashboard.putBoolean("Shooter PID Enabled", true);
+		}
+		return true;
+		
+	}
+	
+	/*
+	 * Methods to move Reset Bar to useful positions
+	 */
+	public void latch() {
+
+		this.setSetpoint(latchPoint);
+		
+		// Save the position
+		m_resetBarSetpoint = latchPoint;
+		
+		if (debugging)
+			SmartDashboard.putNumber("Shooter Setpoint", m_resetBarSetpoint);
+	}
+	
+	public void clear() {
+
+		this.setSetpoint(clearPoint);
+		
+		// Save the position
+		m_resetBarSetpoint = clearPoint;
+		
+		if (debugging)
+			SmartDashboard.putNumber("Shooter Setpoint", m_resetBarSetpoint);
+	}
+	
+	
+	
+	// Control the solenoid that latches the catapult
+	public void cataLatch() {
+		catLatch.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public void cataShoot() {
+		catLatch.set(DoubleSolenoid.Value.kReverse);		
+	}
+
+	public void cataStop() {
+		catLatch.set(DoubleSolenoid.Value.kOff);		
+	}
+
+	// PIDController methods
 	protected double returnPIDInput() {
-		return pot.get();
+		double angle = resetAngle.get();
+		if (debugging) {
+	    	SmartDashboard.putBoolean("Shooter PID Enabled", true);
+	    	SmartDashboard.putNumber("Shooter Reset Angle", angle);
+		}
+		return angle;
 	}
 
 	protected void usePIDOutput(double output) {
-		SmartDashboard.putNumber("Shooter SetPoint", this.getSetpoint());
-		can.set(output);
-	}
-
-	protected void initDefaultCommand() {
-		this.setDefaultCommand(null);
+		if (debugging) {
+			SmartDashboard.putNumber("Shooter SetPoint", this.getSetpoint());
+		}
+		resetBar.set(output);
 	}
 
 }
---
+++ src/org/usfirst/frc/team3467/robot/subsystems/Shooter/commands/ShooterClear.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team3467.robot.subsystems.Shooter.commands;
+
+import org.usfirst.frc.team3467.robot.commands.CommandBase;
+
+
+/**
+ *
+ */
+public class ShooterClear extends CommandBase {
+
+    public ShooterClear() {
+        requires(pultaCat);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	pultaCat.initPIDMode();
+    	pultaCat.clear();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return pultaCat.onTarget();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
