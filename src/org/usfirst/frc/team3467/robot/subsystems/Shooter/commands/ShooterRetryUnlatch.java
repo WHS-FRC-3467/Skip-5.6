82cfbdfe567d7bbc912c2f2b5b89fdec2435bfd3
Scott Kukshtel <scott@kukshtel.com>
2016-02-27 22:18:55 -0500

    Some shooter safety improvements

--- src/org/usfirst/frc/team3467/robot/subsystems/Shooter/commands/ShooterRetryUnlatch.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/Shooter/commands/ShooterRetryUnlatch.java
@@ -4,18 +4,21 @@ import org.usfirst.frc.team3467.robot.commands.CommandBase;
 
 
 /**
- *
+ * Move the shooter latch in an attempt to free a stuck latch or just
+ * shoot regardless of potentiometer position.
+ * Should only be used as a LAST RESORT
  */
 public class ShooterRetryUnlatch extends CommandBase {
 
     public ShooterRetryUnlatch() {
         requires(pultaCat);
+        setTimeout(1.0);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	//Try to relatch...
     	pultaCat.cataLatch();
-    	pultaCat.cataShoot();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -24,11 +27,13 @@ public class ShooterRetryUnlatch extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	// Try to unlatch...
+    	pultaCat.cataShoot();
     }
 
     // Called when another command which requires one or more of the same
