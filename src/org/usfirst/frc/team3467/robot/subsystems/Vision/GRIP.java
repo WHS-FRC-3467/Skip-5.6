ce5f5db56de81cc73c89cdbfe8edcd7b0b5e64db
Starguy18 <hrouillard@team3467.org>
2016-03-01 15:16:25 -0500

    GRIP and Vision Updates (3/1/2016)
    Additions to GRIP, but constants and calibrations need to be adjusted.

--- src/org/usfirst/frc/team3467/robot/subsystems/Vision/GRIP.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/Vision/GRIP.java
@@ -6,27 +6,64 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class GRIP {
 	
 	public NetworkTable table;
+	private double TOLERANCE = 0.0;
+	
+	//is the center of the goal, within the target ranges?
+	boolean onTargetx;
+	boolean onTargety;
+	
+	//Is the goal on target
 	public boolean imageOnTarget;
+				
+	double targetx = 150.1;
+	double targety = 0.0;
+			
+	double[] functionx = new double [0];
+	double[] functiony = new double [0];
+	
+	double[] defaultValue = new double[0];
 	
 	public void createImage () {
-		double[] defaultValue = new double[0];
+		
 		while (true) {
 			double[] centerx = table.getNumberArray("centerX", defaultValue);
-			
-			double center = 150.1;
-			double[] difference;
-			double[] function = new double [0];
-			
-			for (double centers : centerx){
-				System.out.println(centers + " ");
-	//			difference = center + centerx;
-			}
+			double[] centery = table.getNumberArray("centerY", defaultValue);
 			
 			for (int i = 0; i <= centerx.length; i++) {
-				function[i] = 25 * Math.cos((9/32) * centerx[i]);
-				//centers[i] = center;
+				functionx[i] = centerx[i];
+				
+				if(functionx[i] >= targetx - TOLERANCE && functionx[i] <= targetx + TOLERANCE) {
+					onTargetx = true;
+				}
+				System.out.println("Centerx" + functionx[i]);
+			}
+			
+			for (int j = 0; j <= centery.length; j++) {
+				functiony[j] = centery[j];
+				
+				if (functiony[j] >= targety)
+				System.out.println("Centery" + functiony[j]);
+			}
+			
+			if (onTargetx && onTargety) {
+				imageOnTarget = true;
 			}
 		}
-	}	
-}
+	}
 	
+	public double[] getCenterX() {
+		return functionx;
+	}
+	
+	public double[] getCenterY() {
+		return functiony;
+	}
+	
+	public boolean onGoalx() {
+		return onTargetx;
+	}
+	
+	public boolean onGoaly() {
+		return onTargety;
+	}
+}	
